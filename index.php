6e07804417370e0599a32d1f20ccc17d0b2ffb1d
Evgeniia Mishchenko <evgeniia.mishchenko@gmail.com>
2017-03-24 00:54:03 +0200

    Back-end failed first test

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SaaS Nomad</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body style="background-color:rgba(0,0,0,0.1);">
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-3">
+        <h1 style="margin-top:50px;font-weight:bold;">Buy SaaS</h1>
+        <form style="margin:0 auto;">
+            <div class="form-group">
+                <label for="buyer_email">Email</label>
+                <input type="email" class="form-control" id="buyer_email" placeholder="buyer@example.com">
+            </div>
+            <button type="submit" class="btn btn-success">Send</button>
+        </form>
+    </div>
+    <div class="col-md-1"></div>
+    <div class="col-md-4">
+        <h1 style="margin-top:50px;font-weight:bold;">Sell SaaS</h1>
+        <form style="margin:0 auto;">
+            <div class="form-group">
+                <label for="seller_name">Name</label>
+                <input type="text" class="form-control" id="seller_name" placeholder="Jane Doe">
+            </div>
+            <div class="form-group">
+                <label for="seller_email">Email</label>
+                <input type="email" class="form-control" id="seller_email" placeholder="seller@example.com">
+            </div>
+            <div class="form-group">
+                <label for="seller_location">Location</label>
+                <input type="text" class="form-control" id="seller_location" placeholder="United States">
+            </div>
+            <div class="form-group">
+                <label for="seller_industry">Industry</label>
+                <input type="text" class="form-control" id="seller_industry" placeholder="Artificial Intelligence">
+            </div>
+            <div class="form-group">
+                <label for="seller_mrr">Revenue</label>
+                <input type="text" class="form-control" id="seller_mrr" placeholder="MRR">
+            </div>
+            <div class="form-group">
+                <label for="employees">Revenue</label>
+                <input type="text" class="form-control" id="employees" placeholder="Number of employees">
+            </div>
+            <div class="form-group">
+                <label for="asking_price">Asking Price</label>
+                <input type="text" class="form-control" id="asking_price" placeholder="Price">
+            </div>
+            <button type="submit" class="btn btn-success">Send</button>
+        </form>
+    </div>
+    <div class="col-md-2"></div>
+</div>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
