3b50e145c80baa386435ead928b4ddccd26385dc
Evgeniia Mishchenko <evgeniia.mishchenko@gmail.com>
2017-03-25 16:58:04 +0200

    Front-end v1, v2

--- admin.php
+++ admin.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SaaS Nomad</title>
+    <title>SaaS Nomad -- Admin</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
