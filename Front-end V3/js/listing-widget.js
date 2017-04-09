fa8e53d36aa4161629dd5db64ea7451f00685a63
Evgeniia Mishchenko <evgeniia.mishchenko@gmail.com>
2017-04-09 21:46:46 +0300

    form fixes

---
+++ Front-end V3/js/listing-widget.js
@@ -0,0 +1,13 @@
+(function(){
+            var menu_trigger = $("[data-card-menu]");
+            var back_trigger = $("[data-card-back]");
+            
+            menu_trigger.click(function(){
+                $(".card, body").toggleClass("show-menu");
+            });    
+               
+            back_trigger.click(function(){
+                $(".card, body").toggleClass("show-menu");
+            });       
+        })();
+
